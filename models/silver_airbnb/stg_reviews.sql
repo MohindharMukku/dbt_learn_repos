d9c951a4a44d32d533b9c427f962683b51d0e60b
legion <mohindhar.mukku@outlook.com>
2025-05-07 13:53:12 +0530

    created 3 models in silver_airbnb

---
+++ models/silver_airbnb/stg_reviews.sql
@@ -0,0 +1,13 @@
+-- stg_reviews
+WITH src_reviews AS ( SELECT
+*
+FROM DEV.BRONZE_AIRBNB.src_reviews
+)
+SELECT
+listing_id,
+date AS review_date, 
+reviewer_name,
+comments AS review_text, 
+sentiment AS review_sentiment
+FROM
+src_reviews
